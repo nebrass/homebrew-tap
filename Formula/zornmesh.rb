91245e1b346ba9871647a95bcd722074bd82c6cf
Nebrass Lamouchi <lnibrass@gmail.com>
2026-04-30 17:26:50 +0400

    zornmesh 0.1.3

--- Formula/zornmesh.rb
+++ Formula/zornmesh.rb
@@ -1,26 +1,26 @@
 class Zornmesh < Formula
   desc "Local-first agent mesh CLI and Model Context Protocol stdio bridge"
   homepage "https://github.com/nebrass/zorn-mesh"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/nebrass/zorn-mesh/releases/download/v0.1.2/zornmesh-aarch64-apple-darwin.tar.gz"
-      sha256 "996d7f8617091e33baaa30ae564274ed590c8c8bd160f99017ff3b803c2806ec"
+      url "https://github.com/nebrass/zorn-mesh/releases/download/v0.1.3/zornmesh-aarch64-apple-darwin.tar.gz"
+      sha256 "ccc1926dd7ba21ffb0c75adf95459eaee5dddd55f91722afdf515a3dfe20eaf1"
     else
-      url "https://github.com/nebrass/zorn-mesh/releases/download/v0.1.2/zornmesh-x86_64-apple-darwin.tar.gz"
-      sha256 "08fa1122b31a1e02ebf22f075a5449c6d0f15044a2bcb00fa1af493c7e3803e0"
+      url "https://github.com/nebrass/zorn-mesh/releases/download/v0.1.3/zornmesh-x86_64-apple-darwin.tar.gz"
+      sha256 "23261e44d119fe7dd879a17d1cbbbf7d511eb57ca9e43664e7972fe8e6136119"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/nebrass/zorn-mesh/releases/download/v0.1.2/zornmesh-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "131ab348fe05603b5b3867b5a9ef5c114eac6884f94ae50c74e985f9ae7b0fee"
+      url "https://github.com/nebrass/zorn-mesh/releases/download/v0.1.3/zornmesh-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "0a33d68520ce749b3c8c97c0c391c1b39e7228c53b1011999e7620c297bbeed4"
     else
-      url "https://github.com/nebrass/zorn-mesh/releases/download/v0.1.2/zornmesh-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "dc88f298dd9c3640530951932319738a6e1a4b78f8d7d9d1658cde5843bfc881"
+      url "https://github.com/nebrass/zorn-mesh/releases/download/v0.1.3/zornmesh-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "74e0998929405375fafc5e2b1468af4a3733229e411b7e89d185441492456dda"
     end
   end
 
@@ -29,6 +29,6 @@ class Zornmesh < Formula
   end
 
   test do
-    assert_match "zornmesh 0.1.2", shell_output("#{bin}/zornmesh --version")
+    assert_match "zornmesh 0.1.3", shell_output("#{bin}/zornmesh --version")
   end
 end
